8893b6b0c4d9849f3466346179b9dad10136bb58
ggokul407 <81040997+ggokul407@users.noreply.github.com>
2021-03-26 14:52:00 +0530

    Add files via upload

---
+++ web devp/Asst.js
@@ -0,0 +1,17 @@
+// console.log(document.getElementsByTagName('h1'));
+
+var ele=document.getElementsByTagName('Div');
+console.log(ele[0].innerHTML);
+
+// ele[0].innerText="Gokulakrishnan D";
+// ele[1].innerText="Gud Day";
+// console.log(ele[1].innerHTML);
+// console.log(ele[0].innerHTML);
+
+var ele=document.getElementsByClassName('gud');
+console.log(ele[0].value);
+
+var ele=document.getElementById('God');
+console.log(ele);
+
+
